6b27f4fc37debb9e49810a9dbc6c6b3a29defc80
heisenberg <kornilovvladislav222@gmail.com>
2023-04-02 16:48:24 +0300

    fix animation

--- src/shared/animations.ts
+++ src/shared/animations.ts
@@ -3,8 +3,8 @@ import { animate, style, transition, trigger } from "@angular/animations";
 export const fadeIn = [
   trigger('fadeIn', [
     transition(':enter', [
-      style({opacity: 0, transform: 'translateY(80%)'}),
-      animate('500ms', style({opacity: 1, transform: 'none'}))
+      style({opacity: 0, transform: 'translateY(100px)'}),
+      animate('500ms', style({opacity: 1, transform: 'translateY(0)'}))
     ])
   ])
 ];
